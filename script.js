7133898df6b7201abd6853851e4eed8abb6ef270
Jefferson Moraes <143403162+JeffMrCastro@users.noreply.github.com>
2023-10-19 17:37:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const SearchButon = document.getElementById('research');
+const Overlay = document.getElementById('modal_overlay');
+
+function searchclickmaneger() {
+  Overlay.classList.add('open_overlay');
+  
+}
+
+SearchButon.addEventListener('click', searchclickmaneger);
