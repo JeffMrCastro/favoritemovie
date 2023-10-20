c441f65ee6eb9ef65d281214d9496aba89fdd22b
Jefferson Moraes <143403162+JeffMrCastro@users.noreply.github.com>
2023-10-20 15:00:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,4 @@ SearchButon.addEventListener('click', searchclickmaneger);
 
 
 
-const OverlayRemove = document.getElementById('modal_overlay')
-const background = document.getElementById('modal_background');
 
-function backgroundclickmaneger() {
-    OverlayRemove.classList.remove('open_overlay');
-}
-background.addEventListener('click', backgroundclickmaneger);
